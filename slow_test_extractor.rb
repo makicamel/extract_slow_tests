8763b6fdce9b4086d08724abe14574b99f30bff8
Maki Kawahara <maki.kawahara@88oct.co.jp>
2020-08-19 08:26:01 +0900

    Use instance variable directly

--- slow_test_extractor.rb
+++ slow_test_extractor.rb
@@ -8,9 +8,6 @@ class SlowTestExtractor
   EXAMPLE_GROUP_PATTERN = /Top (\d+) slowest example groups:/.freeze
   TIME_PATTERN = %r{(\d+\.?\d*) seconds (\./spec/.*)}.freeze
 
-  attr_accessor :collection
-  attr_reader :log
-
   def initialize(file_name = "result.log")
     @log = File.read(Pathname.pwd + file_name)
     @collection = SlowTestCollection.new
@@ -18,7 +15,7 @@ class SlowTestExtractor
 
   def execute(directory = "results")
     pos = 0
-    log.scan(EXAMPLE_PATTERN).size.times { |_| pos = extract_tests(pos) }
+    @log.scan(EXAMPLE_PATTERN).size.times { |_| pos = extract_tests(pos) }
 
     path_name = Pathname.pwd + directory + "#{Date.today.strftime('%Y%m%d')}.md"
     File.open(path_name, "w") do |file|
@@ -29,9 +26,9 @@ class SlowTestExtractor
   private
 
   def extract_tests(pos)
-    first = log.match(EXAMPLE_PATTERN, pos).end 0
-    last = log.match(EXAMPLE_GROUP_PATTERN, pos).begin 0
-    specs = log[first...last].split("\n").map(&:strip).reject { |match| match == "" }
+    first = @log.match(EXAMPLE_PATTERN, pos).end 0
+    last = @log.match(EXAMPLE_GROUP_PATTERN, pos).begin 0
+    specs = @log[first...last].split("\n").map(&:strip).reject { |match| match == "" }
 
     example = ""
     specs[1..].map.with_index(0) do |spec, i|
@@ -39,14 +36,14 @@ class SlowTestExtractor
         example = spec
       else
         data = spec.match(TIME_PATTERN)
-        collection.push(SlowTest.new(example: example, seconds: data[1], location: data[2]))
+        @collection.push(SlowTest.new(example: example, seconds: data[1], location: data[2]))
       end
     end
     last + 1
   end
 
   def profile_count
-    @profile_count ||= log.match(example_pattern)[1]
+    @profile_count ||= @log.match(example_pattern)[1]
   end
 end
 
